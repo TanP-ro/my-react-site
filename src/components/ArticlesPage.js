fe92108879d48a055652b7621675e88212d93ee5
TanP-ro <pronina19989@gmail.com>
2025-10-09 16:30:01 +0300

    articlepafe1

--- src/components/ArticlesPage.js
+++ src/components/ArticlesPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { HashRouter as Router, Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
 
 function ArticlesPage({ isAdmin }) {
   const [articles, setArticles] = useState([]);
